b1e7f5085bcce811e39028a5345d4cf5ec5247f4
Marko Kosyk <kosyk.markian@gmail.com>
2022-11-03 12:57:38 +0200

    Lab

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Navigation from './components';
+import Main_page from './components';
+
+ const App = function App() {
+  return (
+    <div className="App">
+   <Navigation/>
+   <Main_page/>
+    
+    </div>
+  
+  );
+}
+
+
+export default App;
